757d98014f007a85a810831f0e07d2245bd50cec
calvarado2004 <carlos-alvarado@outlook.com>
2023-03-16 18:45:12 -0400

    adding JSON utilities

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -19,7 +19,20 @@ func (app *application) routes() http.Handler {
 	mux.Post("/v1/auth", app.authenticate)
 	mux.Post("/v1/refresh", app.refresh)
 
-	// test handler
+	// test handler - unprotected route for JSON response
+	mux.Get("/v1/test", func(w http.ResponseWriter, r *http.Request) {
+		var payload = struct {
+			Message string `json:"message"`
+		}{
+			Message: "Hello World!",
+		}
+
+		err := app.writeJSON(w, http.StatusOK, payload, "response")
+		if err != nil {
+			app.errorJSON(w, err)
+		}
+
+	})
 
 	// protected routes
 	mux.Route("/v1/users", func(muxAuth chi.Router) {
