2d45231859651648f5c859665cef013fe1a727ff
calvarado2004 <carlos-alvarado@outlook.com>
2023-03-18 00:24:53 -0400

    authenticate api handler function

--- cmd/api/api-handlers.go
+++ cmd/api/api-handlers.go
@@ -1,19 +1,54 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+)
+
+type Credentials struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 // authenticate handles the authentication process for existing users.
 func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
 
 	// read a json payload
+	err := app.readJSON(w, r, &creds)
+	if err != nil {
+		app.errorJSON(w, errors.New("unable to parse json"), http.StatusUnauthorized)
+		return
+	}
 
 	// look up the user by email address
+	user, err := app.DB.GetUserByEmail(creds.Email)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusUnauthorized)
+		return
+	}
 
 	// check the password matches
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(creds.Password))
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusUnauthorized)
+		return
+	}
 
 	// create a new token for the user
+	tokenPairs, err := app.generateTokenPair(user)
+	if err != nil {
+		app.errorJSON(w, errors.New("unable to generate token"), http.StatusInternalServerError)
+		return
+	}
 
 	// write the token to the response
+	err = app.writeJSON(w, http.StatusOK, tokenPairs, "token")
+	if err != nil {
+		app.errorJSON(w, errors.New("unable to write json"), http.StatusInternalServerError)
+		return
+	}
 
 }
 
